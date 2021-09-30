9c3f0ae3f7d1cbb0dc7de962d2964a0b4b99e9ec
Aneckon <83111616+Aneckon@users.noreply.github.com>
2021-09-30 16:01:23 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-AOS.init();
+AOS.init({disable: 'mobile'});
+
+$(document).ready(function() {
+  $('.burger__menu').click(function() {
+      $('.header__list').toggleClass('open_menu');
+      $('.burger__menu').toggleClass('open_img');
+  });
+});
